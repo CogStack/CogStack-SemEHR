0f20376d2222dc2a4e0b598650597ba5a5fdc4fd
Honghan Wu <honghan.wu@gmail.com>
2016-10-14 12:07:44 +0100

    Update prospector.py
    added the url and template

--- prospector.py
+++ prospector.py
@@ -1,11 +1,12 @@
 import utils
 
 # gate prospector rpc url
-prospector_url = ''
+prospector_url = 'http://192.168.100.101:8080/gwt/gate.prospector.rpc.ProspectorRpc/rpc'
 
 
 # the strange prospector query template
 prospector_query_temp="""
+7|0|7|http://192.168.100.101:8080/gwt/gate.prospector.Prospector/|86F4D95DC3510B0A551734E474D9BBC7|gate.prospector.rpc.client.ProspectorRpcService|search|gate.prospector.rpc.client.SearchSpec/996986553|dcf68f8f-14b1-49e9-ab44-3380eec0a22f|{}|1|2|3|4|1|5|5|6|7|
 """
 
 
