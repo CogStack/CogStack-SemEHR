c19c9b5dfe8c61b66b9e46f0fe953b152f5e25f1
honghan <honghan.wu@gmail.com>
2016-12-01 11:29:16 +0000

    a better read function

--- autoimmune.py
+++ autoimmune.py
@@ -24,17 +24,12 @@ autoimmune_sympton_freq_sql = """
 """
 
 
-def read_data(rows, container):
-    print dir(rows[0])
-    container += [r.__dict__ for r in rows]
-
-
 def get_concepts(output_file):
     autoimmune_concepts = []
     patients = []
-    dutil.query_data(autoimmune_concepts_sql, read_data, autoimmune_concepts)
+    dutil.query_data(autoimmune_concepts_sql, autoimmune_concepts)
     print '{} concepts read'.format(len(autoimmune_concepts))
-    dutil.squery_data(autoimmune_concepts_sql, read_data, patients)
+    dutil.squery_data(autoimmune_concepts_sql, patients)
     # patient dic
     patient_dic = {}
     for p in patients:
@@ -42,7 +37,7 @@ def get_concepts(output_file):
 
     for c in autoimmune_concepts:
         sympton_freq_result = []
-        dutil.squery_data(autoimmune_sympton_freq_sql.format(c), read_data, sympton_freq_result)
+        dutil.squery_data(autoimmune_sympton_freq_sql.format(c), sympton_freq_result)
         for sf in sympton_freq_result:
             patient_dic[sf['BrcId']][c] = sf['num']
     s = '\t'.join([k for k in patients[0]] + autoimmune_concepts) + '\n'
