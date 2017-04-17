d3100385c875a062ed4dbce35d6382d5af80a53c
Honghan Wu <honghan.wu@gmail.com>
2017-04-17 18:42:10 +0100

    Update mimicdao.py

--- mimicdao.py
+++ mimicdao.py
@@ -7,7 +7,7 @@ import datetime
 import json
 
 # db connection string
-db_cnn_str = "dbname='mimic' user='mimic' password='123321!a' host='localhost'"
+db_cnn_str = "dbname='mimic' user='mimic' password='' host='localhost'"
 
 # query templates
 # patient cohort by diagnosis template
