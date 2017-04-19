7309c19a5cacdfab5dbe322191ac7d7242d9068d
honghan <honghan.wu@gmail.com>
2017-04-19 15:19:01 +0100

    check before indexing entity

--- entity_centric_es.py
+++ entity_centric_es.py
@@ -224,6 +224,7 @@ class EntityCentricES(object):
             doc_entity = self._es_instance.get(self.index_name, entity_id, doc_type=self.entity_doc_type)
             if 'found' in  doc_entity and doc_entity['found']:
                 print 'entity %s exists' % entity_id
+                return
         results = self._es_instance.search(index=self.index_name,
                                            doc_type=self.doc_doc_type,
                                            body={'query': {'term': {entity_field_id: entity_id}}, 'size': 10000})
