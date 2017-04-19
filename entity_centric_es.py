ae9cecfa4a675637dbcf8c9649a5d23e3425aa0c
honghan <honghan.wu@gmail.com>
2017-04-19 15:26:52 +0100

    check before indexing entity

--- entity_centric_es.py
+++ entity_centric_es.py
@@ -221,7 +221,8 @@ class EntityCentricES(object):
     
     def query_entity_to_index(self, entity_id, entity_field_id='patientId', date_field=None, check_before_index=False):
         if check_before_index:
-            doc_entity = self._es_instance.get(self.index_name, entity_id, doc_type=self.entity_doc_type)
+            doc_entity = self._es_instance.get(self.index_name, entity_id, doc_type=self.entity_doc_type,
+                                               _source_include='a')
             if 'found' in  doc_entity and doc_entity['found']:
                 print 'entity %s exists' % entity_id
                 return
