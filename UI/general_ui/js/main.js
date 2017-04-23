6f81accebeb2d58a6dea9bbd119afff848cb2fe8
honghan <honghan.wu@gmail.com>
2017-04-23 22:44:33 +0100

    general semehr UI implemented based on mimic data

--- UI/general_ui/js/main.js
+++ UI/general_ui/js/main.js
@@ -425,6 +425,7 @@
 
             $("#chartDivOverlay").hide();
         });
+        $('#chartToolTip').html('top ' + data.length + ' concepts (other than you searched)');
 
         var svg = d3.select("svg"),
             margin = {top: 20, right: 20, bottom: 30, left: 40},
