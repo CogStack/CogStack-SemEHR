ad881befa01a046d877d7e80de860b84c1131637
Honghan Wu <honghan.wu@gmail.com>
2017-06-16 14:23:13 +0100

    semehr index based study implementations

---
+++ analysis/addiction_use_case.py
@@ -0,0 +1,90 @@
+from semquery import SemEHRES
+from datetime import timedelta, datetime
+import utils
+from os.path import isfile, join
+from os import listdir
+import json
+
+
+def time_window_filtering(p2time, d2time, ann, patient):
+    d_time = None if ann['appearances'][0]['eprid'] not in d2time else d2time[ann['appearances'][0]['eprid']]
+    p_time = None if patient['_id'] not in p2time else p2time[patient['_id']]
+    if d_time is None or p_time is None:
+        return True # TODO: discussion needed here, the default action when no time info is available
+    else:
+        return timedelta(days=-365) + p_time <= d_time <= timedelta(days=7) + p_time
+
+
+def load_patient_date(patient_date_file):
+    lines = utils.read_text_file(patient_date_file)
+    print 'patient file read. parsing...'
+    p2time = {}
+    for l in lines[1:]:
+        arr = l.split(',')
+        p2time[arr[0]] = datetime.strptime(arr[1], '%d/%m/%Y')
+    return p2time
+
+
+def load_doc_date(doc_date_file):
+    lines = utils.read_text_file(doc_date_file)
+    print 'doc file read. parsing...'
+    d2time = {}
+    for l in lines[1:]:
+        arr = l.split('\t')
+        d2time[arr[0]] = datetime.strptime(arr[2], '%Y-%m-%d %H:%M:%S.%f')
+    return d2time
+
+
+def query_hepc_results():
+    # load patient and document dates
+    p2time = load_patient_date('U:/kconnect/complete_addiction_cohort_entry_date.csv')
+    d2time = load_doc_date('U:/kconnect/doc_dates.txt')
+    es = SemEHRES.get_instance()
+    results = es.summary_patients_by_concepts(['C0019196', 'C2148557', 'C0220847'], time_window_filtering,
+                                              args=[p2time, d2time])
+    utils.save_json_array(results, './addiction_res/hepc_results.json')
+
+
+def query_drugs_results(drug, concepts):
+    # load patient and document dates
+    es = SemEHRES.get_instance()
+    results = es.summary_patients_by_concepts(concepts)
+    utils.save_json_array(results, './addiction_res/%s_results.json' % drug)
+
+
+def merge_and_output(dir_path):
+    headers = ['all', 'positive', 'Negated', 'hypothetical', 'historical', 'Other']
+
+    results = {}
+    c_results = utils.load_json_data(join(dir_path, 'hepc_results.json'))
+    for p in c_results:
+        results[p['id']] = p
+    for f in [f for f in listdir(dir_path) if isfile(join(dir_path, f))]:
+        c_results = utils.load_json_data(join(dir_path, f))
+        if f != 'hepc_results.json':
+            d = f.replace('_results.json', '')
+            if d not in headers:
+                headers.append(d)
+            for p in c_results:
+                results[p['id']][d] = p['all']
+
+    s = '\t'.join(['id'] + headers) + '\n'
+    for pid in results:
+        p = results[pid]
+        row = [pid] + ['-' if h not in p else str(p[h]) for h in headers]
+        s += '\t'.join(row) + '\n'
+    print s
+
+if __name__ == "__main__":
+    # query_hepc_results()
+    # query_drugs_results('RIBAVIRIN', ['C1382829', 'C1128545', 'C0035525'])
+    # query_drugs_results('PEGINTERFERON ALPHA',
+    #                     ['C0982327','C0907160','C0279030','C0021747','C2599808','C0021734','C0002199','C3165060'])
+    # query_drugs_results('SOFOSBUVIR', ['C2976303'])
+    # query_drugs_results('DACLATASVIR', ['C3252090'])
+    # query_drugs_results('LEDIPASVIR', ['C3851350'])
+    # query_drugs_results('RITONAVIR', ['C0992889','C0939237','C0292818','C2741195','C1676707'])
+    # query_drugs_results('BOCEPREVIR', ['C1738934'])
+    # query_drugs_results('TELAPREVIR', ['c1876229'])
+    # print(timedelta(days=-365) + datetime.strptime('2016-02-08 00:00:00.000', '%Y-%m-%d %H:%M:%S.%f'))
+    merge_and_output('./addiction_res/')
---
+++ analysis/semquery.py
@@ -0,0 +1,88 @@
+from elasticsearch import Elasticsearch, RequestsHttpConnection, serializer, compat, exceptions
+from datetime import timedelta, datetime
+import utils
+
+_es_host = 'localhost'
+_es_index = 'hepc_unknown'
+_doc_type = 'eprdoc'
+_concept_type = 'ctx_concept'
+_patient_type = 'patient'
+_es_instance = None
+_page_size = 100
+
+
+class SemEHRES(object):
+    def __init__(self, es_host, es_index, doc_type, concept_type, patient_type):
+        self._host = es_host
+        self._es_instance = Elasticsearch([self._host])
+        self._index = es_index
+        self._doc_type = doc_type
+        self._patient_type = patient_type
+        self._concept_type = concept_type
+        self._customise_settings = None
+
+    def search_patient(self, q):
+        patients = []
+        need_next_query = True
+        offset = 0
+        while need_next_query:
+            results = self._es_instance.search(self._index, self._patient_type, {"query": {"match": {"_all": q}},
+                                                                                 "from": offset,
+                                                                                 "size": _page_size})
+            total = results['hits']['total']
+            for p in results['hits']['hits']:
+                patients.append(p)
+            offset += len(results['hits']['hits'])
+            if offset >= total:
+                need_next_query = False
+        return patients
+
+    def get_contexted_concepts(self, concept):
+        results = self._es_instance.search(self._index, self._concept_type, {"query": {"match": {"_all": concept}},
+                                                                             "size": 100
+                                                                             })
+        cc_to_ctx = {}
+        for cc in results['hits']['hits']:
+            d = cc['_source']
+            cid = cc['_id']
+            if d['experiencer'] == 'Other':
+                cc_to_ctx[cid] = 'Other'
+            elif d['temporality'] == 'historical':
+                cc_to_ctx[cid] = 'historical'
+            elif d['temporality'] == 'hypothetical':
+                cc_to_ctx[cid] = 'hypothetical'
+            elif d['negation'] == 'Negated':
+                cc_to_ctx[cid] = 'Negated'
+            else:
+                cc_to_ctx[cid] = 'positive'
+        return cc_to_ctx
+
+    def summary_patients_by_concepts(self, concepts, filter_func=None, args=[]):
+        cc_to_ctx = {}
+        for t in concepts:
+            cc_to_ctx.update(self.get_contexted_concepts(t))
+        print len(cc_to_ctx)
+        patients = self.search_patient(' '.join(concepts))
+        results = []
+        for p in patients:
+            sp = {'id': p['_id'], 'all': 0}
+            for ann in p['_source']['anns']:
+                if ann['contexted_concept'] in cc_to_ctx:
+                    if filter_func is not None:
+                        # do filter, if filter function returns false, skip it
+                        if not filter_func(*tuple(args + [ann, p])):
+                            continue
+                    t = cc_to_ctx[ann['contexted_concept']]
+                    sp[t] = 1 if t not in sp else sp[t] + 1
+                    sp['all'] += 1
+            results.append(sp)
+        return results
+
+    @staticmethod
+    def get_instance():
+        global _es_instance
+        if _es_instance is None:
+            _es_instance = SemEHRES(_es_host, _es_index, _doc_type, _concept_type, _patient_type)
+        return _es_instance
+
+
