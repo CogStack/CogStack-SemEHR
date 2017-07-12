5ce95b01eb56e7f047ec58192f86986d84ba7a97
Honghan Wu <honghan.wu@gmail.com>
2017-07-12 21:08:35 +0100

    Update addiction_use_case.py

--- analysis/addiction_use_case.py
+++ analysis/addiction_use_case.py
@@ -35,15 +35,20 @@ def load_doc_date(doc_date_file):
     return d2time
 
 
-def query_hepc_results():
+def read_patient_time_windows(time_file_loc):
     # load patient and document dates
-    p2time = load_patient_date('U:/kconnect/complete_addiction_cohort_entry_date.csv')
-    d2time = load_doc_date('U:/kconnect/doc_dates.txt')
+    p2time = load_patient_date('%s/complete_addiction_cohort_entry_date.csv' % time_file_loc)
+    patient_filter = [p for p in p2time]
+    d2time = load_doc_date('%s/doc_dates.txt' % time_file_loc)
+    return patient_filter, p2time, d2time
+
+
+def query_hepc_results(concepts, prefix, patient_filter, p2time, d2time):
     es = SemEHRES.get_instance()
-    results, docs = es.summary_patients_by_concepts(['C0019196', 'C2148557', 'C0220847'], time_window_filtering,
-                                              args=[p2time, d2time])
-    utils.save_json_array(results, './addiction_res/hepc_results.json')
-    utils.save_json_array(docs, './addiction_res/hepc_valid_docs.json')
+    results, docs = es.summary_patients_by_concepts(concepts, time_window_filtering,
+                                                    args=[p2time, d2time], patient_filters=patient_filter)
+    utils.save_json_array(results, './addiction_res/%s_results.json' % prefix)
+    utils.save_json_array(docs, './valid_doc_files/%s_valid_docs.json' % prefix)
 
 
 def query_drugs_results(drug, concepts):
@@ -53,17 +58,20 @@ def query_drugs_results(drug, concepts):
     utils.save_json_array(results, './addiction_res/%s_results.json' % drug)
 
 
-def merge_and_output(dir_path):
+def merge_and_output(dir_path, cohort, default_results='hepc_results.json'):
     headers = ['all', 'positive', 'Negated', 'hypothetical', 'historical', 'Other']
 
     results = {}
-    c_results = utils.load_json_data(join(dir_path, 'hepc_results.json'))
+    for pid in cohort:
+        results[pid] = {}
+    c_results = utils.load_json_data(join(dir_path, default_results))
     for p in c_results:
         results[p['id']] = p
     for f in [f for f in listdir(dir_path) if isfile(join(dir_path, f))]:
-        c_results = utils.load_json_data(join(dir_path, f))
-        if f != 'hepc_results.json':
+        if f != default_results:
+            c_results = utils.load_json_data(join(dir_path, f))
             d = f.replace('_results.json', '')
+            print f
             if d not in headers:
                 headers.append(d)
             for p in c_results:
@@ -74,29 +82,23 @@ def merge_and_output(dir_path):
         p = results[pid]
         row = [pid] + ['-' if h not in p else str(p[h]) for h in headers]
         s += '\t'.join(row) + '\n'
-    print s
-
-
-def print_manual_checked_liver_concepts():
-    concepts = utils.load_json_data('./concept_maps/liver_disease_concepts.json')
-    checked = utils.load_json_data('./concept_maps/liver_diseases_checked.json')
-    chked_concepts = []
-    for cname in checked:
-        if checked[cname] == 'correct':
-            chked_concepts.append(concepts[cname]['mapped'])
-    print json.dumps(chked_concepts)
+    utils.save_string(s, './valid_doc_files/merged_output_liverdiseases_7d.tsv')
+    print 'output generated'
 
 if __name__ == "__main__":
-    # query_hepc_results()
-    # query_drugs_results('RIBAVIRIN', ['C1382829', 'C1128545', 'C0035525'])
-    # query_drugs_results('PEGINTERFERON ALPHA',
-    #                     ['C0982327','C0907160','C0279030','C0021747','C2599808','C0021734','C0002199','C3165060'])
-    # query_drugs_results('SOFOSBUVIR', ['C2976303'])
-    # query_drugs_results('DACLATASVIR', ['C3252090'])
-    # query_drugs_results('LEDIPASVIR', ['C3851350'])
-    # query_drugs_results('RITONAVIR', ['C0992889','C0939237','C0292818','C2741195','C1676707'])
-    # query_drugs_results('BOCEPREVIR', ['C1738934'])
-    # query_drugs_results('TELAPREVIR', ['c1876229'])
-    # print(timedelta(days=-365) + datetime.strptime('2016-02-08 00:00:00.000', '%Y-%m-%d %H:%M:%S.%f'))
-    # merge_and_output('./addiction_res/')
-    print_manual_checked_liver_concepts()
+    ps, p2time, d2time = read_patient_time_windows('.')
+    pids = utils.read_text_file('./valid_doc_files/hcv_full_cohort.csv')
+    ps = pids
+    query_hepc_results(["C0679412", "C0019187", "C0400966", "C0020541", "C0015695", "C2718067", "C0015695", "C0151763", "C2711227", "C0023896", "C0023892", "C0152254", "C0023891", "C0001306", "C0238065", "C0085605", "C0524610", "C0015696", "C0341439", "C0348754", "C0023890"]
+, 'liver_diseases', ps, p2time, d2time)
+    # query_hepc_results(['C0019196', 'C2148557', 'C0220847'], 'hepc', ps, p2time, d2time)
+    #query_hepc_results(['C1382829', 'C1128545', 'C0035525'], 'RIBAVIRIN', ps, p2time, d2time)
+    #query_hepc_results(['C0982327','C0907160','C0279030','C0021747','C2599808','C0021734','C0002199','C3165060'],
+    #                   'PEGINTERFERON ALPHA', ps, p2time, d2time)
+    #query_hepc_results(['C2976303'], 'SOFOSBUVIR', ps, p2time, d2time)
+    #query_hepc_results(['C3252090'], 'DACLATASVIR', ps, p2time, d2time)
+    #query_hepc_results(['C3851350'], 'LEDIPASVIR', ps, p2time, d2time)
+    #query_hepc_results(['C0992889','C0939237','C0292818','C2741195','C1676707'], 'RITONAVIR', ps, p2time, d2time)
+    #query_hepc_results(['C1738934'], 'BOCEPREVIR', ps, p2time, d2time)
+    #query_hepc_results(['c1876229'], 'TELAPREVIR', ps, p2time, d2time)
+    merge_and_output('./addiction_res/', pids, 'liver_diseases_results.json')
