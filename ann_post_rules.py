4871a7857dc96f8f47f5fbe4c28bc1a0696c3a02
honghan <honghan.wu@gmail.com>
2018-04-24 17:10:42 +0100

    supporting mention orginal string rules

--- ann_post_rules.py
+++ ann_post_rules.py
@@ -12,6 +12,7 @@ class AnnRuleExecutor(object):
         self._text_window = _text_window
         self._filter_rules = []
         self._skip_terms = []
+        self._osf_rules = []
 
     @property
     def skip_terms(self):
@@ -21,8 +22,8 @@ class AnnRuleExecutor(object):
     def skip_terms(self, value):
         self._skip_terms = value
 
-    def add_filter_rule(self, token_offset, reg_strs):
-        self._filter_rules.append({'offset': token_offset, 'regs': reg_strs})
+    def add_filter_rule(self, token_offset, reg_strs, case_sensitive=False):
+        self._filter_rules.append({'offset': token_offset, 'regs': reg_strs, 'case_sensitive': case_sensitive})
 
     def execute(self, text, ann_start, ann_end):
         s_before = text[max(ann_start - self._text_window, 0):ann_start]
@@ -39,7 +40,10 @@ class AnnRuleExecutor(object):
                 s_compare = text[:_head_text_window_size]
             s_compare = s_compare.replace('\n', ' ')
             try:
-                reg_p = re.compile('|'.join(r['regs']), re.IGNORECASE)
+                if r['case_sensitive']:
+                    reg_p = re.compile('|'.join(r['regs']))
+                else:
+                    reg_p = re.compile('|'.join(r['regs']), re.IGNORECASE)
             except Exception:
                 print 'regs error: [%s]' % r['regs']
                 exit(1)
@@ -48,6 +52,33 @@ class AnnRuleExecutor(object):
             if m is not None:
                 # print m.group(0)
                 matched.append([m.group(0), r['regs']])
+                filtered = True
+                break
+        return filtered, matched
+
+    def add_original_string_filters(self, regs):
+        self._osf_rules += regs
+
+    def execute_original_string_rules(self, string_orig):
+        """
+        filter the matching substring using patterns
+        :param string_orig:
+        :return:
+        """
+        s_compare = string_orig
+        filtered = False
+        matched = []
+        for r in self._osf_rules:
+            try:
+                reg_p = re.compile(r)
+            except Exception:
+                print 'regs error: [%s]' % r['regs']
+                exit(1)
+            # print 'matching %s on %s' % (reg_p, s_compare)
+            m = reg_p.match(s_compare)
+            if m is not None:
+                # print m.group(0)
+                matched.append([m.group(0), r])
                 filtered = True
                 break
         return filtered, matched
@@ -64,11 +95,24 @@ class AnnRuleExecutor(object):
             self.skip_terms = utils.load_json_data(rule_config['skip_term_setting'])
 
 
-if __name__ == "__main__":
+def test_filter_rules():
     t = "\na close\n frined of hers died of cancer"
+    t = "aaa has No evidence of cancer"
     e = AnnRuleExecutor()
-    rules = utils.load_json_data('./studies/autoimmune.v2/post_filter_rules.json')
+    rules = utils.load_json_data('./studies/rules/negation_filters.json')
     for r in rules:
         print r
-        e.add_filter_rule(r['offset'], r['regs'])
-    print e.execute(t, 33,38)
+        e.add_filter_rule(r['offset'], r['regs'], case_sensitive=True if 'case' in r and r['case'] is True else False)
+    print e.execute(t, 24, 30)
+
+
+def test_osf_rules():
+    t = "ADAD-A"
+    e = AnnRuleExecutor()
+    rules = utils.load_json_data('./studies/rules/osf_acroynm_filters.json')
+    e.add_original_string_filters(rules)
+    print e.execute_original_string_rules(t)
+
+
+if __name__ == "__main__":
+    test_osf_rules()
