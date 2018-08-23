418adbbc7869b14d763d1b0a8e7e5b333111a963
Honghan <honghan.wu@gmail.com>
2018-08-23 10:22:52 +0100

    a stupid bug fixed

--- analysis/text2phenome/data_analyzer.py
+++ analysis/text2phenome/data_analyzer.py
@@ -366,7 +366,7 @@ class MConcept(object):
         c_sorted = sorted([l for l in self.labels], key=lambda x: -x.condition_mention)
         k_freq = 0
         t_freq = 0
-        for i in len(c_sorted):
+        for i in xrange(len(c_sorted)):
             if i + 1 <= k:
                 k_freq += c_sorted[i-1]['condition_mention']
             t_freq += c_sorted[i-1]['condition_mention']
