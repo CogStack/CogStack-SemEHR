1e2b546c7cc5c3f370e69226e1eb87344b6fb329
honghan <honghan.wu@gmail.com>
2018-12-19 23:07:18 +0000

    using sample docs when no docs found in input dir

--- docker/semehr.sh
+++ docker/semehr.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-sesetting=/data/semehr_settings.json
+datapath=/data
+sesetting="$datapath/semehr_settings.json"
 if [ ! -f "$sesetting" ]; then
     echo "setting file not found at/data/emehr_settings.json, use default settings in SemEHR repo"
     sesetting="$semehr_path/docker/docker_doc_based_settings.json"
+fi
+
+#check input docs
+ndocs=`find $datapath/input_docs/ -maxdepth 1 -type f | wc -l`
+if [[ $ndocs = 0 ]]; then
+    echo 'no files found, copying sample docs...'
+    cp "$semehr_path/resources/sample_docs/*" /data/input_docs
+else
+    echo "total $ndocs docs to process..."
 fi
 python "$semehr_path/semehr_processor.py" "$sesetting"
