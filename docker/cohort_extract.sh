2b5a06df59ebbd77bc9cf18aef122da95d88760f
honghan <honghan.wu@gmail.com>
2018-12-27 22:06:36 +0000

    update repo before executing

--- docker/cohort_extract.sh
+++ docker/cohort_extract.sh
@@ -2,11 +2,14 @@
 
 COHORTCONF=/data/cohort.json
 
+cd "$semehr_path"
+echo "update repo..."
+git pull
+
 if [[ ! -f "$COHORTCONF" ]]; then
     echo "cohort configuration file not found at $COHORTCONF"
     exit -1
 fi
 
-cd "$semehr_path"
 echo "extracting docs using $COHORTCONF"
 python cohort_helper.py "$COHORTCONF"
