83e0be00fda485834efe3fc9a95c0254b3af6bd3
honghan <honghan.wu@gmail.com>
2018-12-30 10:41:59 +0000

    dump string before escaping

--- docanalysis.py
+++ docanalysis.py
@@ -363,7 +363,7 @@ def db_doc_process(row, sql_template, pks, update_template, dbcnn_file, sa, rule
         if len(ann_doc.annotations) > 0:
             text = rets[0]['text']
             process_doc_rule(ann_doc, ruler, text, sa)
-            update_query = update_template.format(*([db.escape_string(ann_doc.serialise_json())] +
+            update_query = update_template.format(*([db.escape_string(json.dumps(ann_doc.serialise_json()))] +
                                                     [row[k] for k in pks]))
             logging.debug('update ann: %s' % update_query)
             db.query_data(update_query, None, db.get_db_connection_by_setting(dbcnn_file))
