89c455fc3f1e5b4b7c4fe166bc039f43236fbfb7
honghan <honghan.wu@gmail.com>
2019-01-02 13:02:20 +0000

    boundary char

--- ann_post_rules.py
+++ ann_post_rules.py
@@ -33,7 +33,7 @@ class AnnRuleExecutor(object):
         if t[s:e] == string_orig:
             return [s, e]
         candidates = []
-        ito = re.finditer(r'[\s\.;\,\?\!\:\/$^](' + string_orig + r')[\s\.;\,\?\!\:\/$^]',
+        ito = re.finditer(r'\b(' + string_orig + r')\b',
                     t, re.IGNORECASE)
         for mo in ito:
             # print mo.start(1), mo.end(1), mo.group(1)
