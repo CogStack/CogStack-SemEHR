12df1ca6bff096730d6e7cb2eee85f40d995e6d8
honghan <honghan.wu@gmail.com>
2019-04-12 11:39:33 +0100

    escape string for text

--- cohort_helper.py
+++ cohort_helper.py
@@ -88,6 +88,7 @@ class CohortHelper(object):
 
     @staticmethod
     def do_save_doc_to_db(d, sql_temp, db_conf):
+        d['doc_content'] = db.escape_string(d['doc_content'])
         sql = sql_temp.format(**d)
         db.query_data(sql, [], dbconn=db_conf)
 
